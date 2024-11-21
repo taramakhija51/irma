96c09cd077fb2d25a9650e6f84b0db2e9722074f
taramakhija51 <114837329+taramakhija51@users.noreply.github.com>
2024-11-21 02:38:04 +0000

    added devise for sign-in

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,10 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many(:contacts,
   class_name: "Contact",
   foreign_key: "user_id"
