c85212e659824e4817868f3a24f8f1c8d1148a4f
taramakhija51 <114837329+taramakhija51@users.noreply.github.com>
2024-11-22 05:59:32 +0000

    hidden

--- config/routes.rb
+++ config/routes.rb
@@ -1,85 +1,36 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Routes for the Interaction resource:
+  
+  # Alias for sign-out
+  get "/sign_out", to: "devise/sessions#destroy"
 
-  # CREATE
+  # Routes for the Interaction resource:
   post("/insert_interaction", { :controller => "interactions", :action => "create" })
-          
-  # READ
   get("/interactions", { :controller => "interactions", :action => "index" })
-  
   get("/interactions/:path_id", { :controller => "interactions", :action => "show" })
-  
-  # UPDATE
-  
   post("/modify_interaction/:path_id", { :controller => "interactions", :action => "update" })
-  
-  # DELETE
   get("/delete_interaction/:path_id", { :controller => "interactions", :action => "destroy" })
 
-  #------------------------------
-
   # Routes for the Event resource:
-
-  # CREATE
   post("/insert_event", { :controller => "events", :action => "create" })
-          
-  # READ
   get("/events", { :controller => "events", :action => "index" })
-  
   get("/events/:path_id", { :controller => "events", :action => "show" })
-  
-  # UPDATE
-  
   post("/modify_event/:path_id", { :controller => "events", :action => "update" })
-  
-  # DELETE
   get("/delete_event/:path_id", { :controller => "events", :action => "destroy" })
 
-  #------------------------------
-
   # Routes for the Contact resource:
-
-  # CREATE
   post("/insert_contact", { :controller => "contacts", :action => "create" })
-          
-  # READ
   get("/contacts", { :controller => "contacts", :action => "index" })
-  
   get("/contacts/:path_id", { :controller => "contacts", :action => "show" })
-  
-  # UPDATE
-  
   post("/modify_contact/:path_id", { :controller => "contacts", :action => "update" })
-  
-  # DELETE
   get("/delete_contact/:path_id", { :controller => "contacts", :action => "destroy" })
 
-  #------------------------------
-
   # Routes for the User resource:
-
-  # CREATE
   post("/insert_user", { :controller => "users", :action => "create" })
-          
-  # READ
   get("/users", { :controller => "users", :action => "index" })
-  
   get("/users/:path_id", { :controller => "users", :action => "show" })
-  
-  # UPDATE
-  
   post("/modify_user/:path_id", { :controller => "users", :action => "update" })
-  
-  # DELETE
   get("/delete_user/:path_id", { :controller => "users", :action => "destroy" })
 
-  #------------------------------
-
-root "contacts#index"
-
-  # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
-
-  # get "/your_first_screen" => "pages#first"
-  
+  root "contacts#index"
 end
