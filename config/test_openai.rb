1198db3470a1746142b2328c75f98ce4602cece0
taramakhija51 <114837329+taramakhija51@users.noreply.github.com>
2025-01-05 20:15:36 +0000

    losing my mind

--- config/test_openai.rb
+++ config/test_openai.rb
@@ -1,17 +1,16 @@
 require 'openai'
+require 'dotenv/load'
 
-OpenAI.configure do |config|
-  config.access_token = ENV['OPENAI_API_KEY']
-end
-
-client = OpenAI::Client.new
+# Pass the API key as :api_key
+puts "OpenAI API Key: #{ENV['OPENAI_API_KEY']}"
+client = OpenAI::Client.new(api_key: ENV['OPENAI_API_KEY'])
 
 texts_to_test = [
   "Met at a conference.",
   "Former boss.",
   "Introduced by a mutual friend.",
   "", # Test empty string
-  "This is a very long string to test the character limit. It should cause an error if the character limit is properly implemented. this is a very long string to test the character limit. It should cause an error if the character limit is properly implemented. this is a very long string to test the character limit. It should cause an error if the character limit is properly implemented. this is a very long string to test the character limit. It should cause an error if the character limit is properly implemented. this is a very long string to test the character limit. It should cause an error if the character limit is properly implemented. this is a very long string to test the character limit. It should cause an error if the character limit is properly implemented."
+  "This is a very long string to test the character limit. It should cause an error if the character limit is properly implemented..."
 ]
 
 texts_to_test.each do |text|
